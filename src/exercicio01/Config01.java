030169f695895d12d042d176631d9fd03b4611e3
pvmeira <pv.ciria@gmail.com>
2017-03-15 11:00:45 -0300

    Adicionando resposta do exercicio03

--- src/exercicio01/Config01.java
+++ src/exercicio01/Config01.java
@@ -6,68 +6,101 @@ import java.util.List;
 import java.util.Random;
 
 public class Config01 {
-	  private int[] resultArray = new int[200];
-	  
-	  public int createRandomNumber(){
-	      
-	    int result;
-	      Random r = new Random();
-	    result = r.nextInt();
-	    
-	      return result;
-	  }
-	  
-	  
-	  public int[] fillTheArray(){
-	   for (int i = 0; i < resultArray.length; i++) {
-	     int currentNumber;
-	     
-	     currentNumber = this.createRandomNumber();
-	     resultArray[i] = currentNumber;
-	   }
-	      return resultArray;
-	      
-	  }
-	  
-	  public void printAllArray(){
-	      int[] r = this.fillTheArray();
-	      
-	      for (int i = 0; i < r.length; i++) {
-	         System.out.println(r[i]);
-	     }
-	  }  
-	  
-	  public void printMaxNumber(int[] r){
-	      List<Integer> list = new ArrayList<Integer>();
-	      for (int i = 0; i < r.length; i++) {
-	     list.add(r[i]);
-	   }
-	      System.out.println( Collections.max(list));
-	  }
-	  
-	  public void printAverageValue(int[] r){
-	      int result = 0;
-	      for (int i = 0; i < r.length; i++) {
-	          result = result+r[i];
-	      }
-	      System.out.println(result/r.length);
-	  }
-	  
-	   
-	  public void printLowNumber(int[] r){
-	      List<Integer> list = new ArrayList<Integer>();
-	      for (int i = 0; i < r.length; i++) {
-	     list.add(r[i]);
-	   }
-	      System.out.println( Collections.min(list));
-	  }
+	private int[] resultArray = new int[200];
 
-	     public int[] getResultArray() {
-	         return resultArray;
-	     }
+	/**
+	 * Create a random number
+	 * 
+	 * @return
+	 */
+	public int createRandomNumber() {
 
-	     public void setResultArray(int[] resultArray) {
-	         this.resultArray = resultArray;
-	     }
-	  
+		int result;
+		Random r = new Random();
+		result = r.nextInt();
+
+		return result;
+	}
+
+	/**
+	 * Fill the array with random numbers
+	 * 
+	 * @return
+	 */
+	public int[] fillTheArray() {
+		for (int i = 0; i < resultArray.length; i++) {
+			int currentNumber;
+
+			currentNumber = this.createRandomNumber();
+			resultArray[i] = currentNumber;
+		}
+		return resultArray;
+
+	}
+
+	/**
+	 * Print the array
+	 */
+	public void printAllArray() {
+		int[] r = this.fillTheArray();
+
+		for (int i = 0; i < r.length; i++) {
+			System.out.println(r[i]);
+		}
+	}
+
+	/**
+	 * Print the max number on the given array
+	 * 
+	 * @param r
+	 */
+	public void printMaxNumber(int[] r) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < r.length; i++) {
+			list.add(r[i]);
+		}
+		System.out.println(Collections.max(list));
+	}
+
+	/**
+	 * print the average value on the given array
+	 * 
+	 * @param r
+	 */
+	public void printAverageValue(int[] r) {
+		int result = 0;
+		for (int i = 0; i < r.length; i++) {
+			result = result + r[i];
+		}
+		System.out.println(result / r.length);
+	}
+
+	/**
+	 * Print the lowest number on the given array
+	 * 
+	 * @param r
+	 */
+	public void printLowNumber(int[] r) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < r.length; i++) {
+			list.add(r[i]);
+		}
+		System.out.println(Collections.min(list));
+	}
+
+	/**
+	 * @return the resultArray
+	 */
+	public int[] getResultArray() {
+		return resultArray;
+	}
+
+	/**
+	 * @param resultArray
+	 *            the resultArray to set
+	 */
+	public void setResultArray(int[] resultArray) {
+		this.resultArray = resultArray;
+	}
+
 }
--- src/exercicio03/Config03.java
+++ src/exercicio03/Config03.java
@@ -3,18 +3,56 @@ package exercicio03;
 import java.util.Random;
 
 public class Config03 {
+	/**
+	 * Create the Random Array
+	 * 
+	 * @return
+	 */
 	public int[] createRandomArray() {
 		int[] result = new int[10];
 		result = fillArray(result);
 		return result;
 	}
 
+	/**
+	 * Print the given Array
+	 * 
+	 * @param r
+	 */
 	public void printArray(int[] r) {
 		for (int i = 0; i < r.length; i++) {
 			System.out.println(r[i]);
 		}
+		System.out.println("Maximum contiguous sum is " + maxSubArraySum(r));
 	}
 
+	/**
+	 * Get the max sub Array
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public int maxSubArraySum(int a[]) {
+		int size = a.length;
+		int result = Integer.MIN_VALUE;
+		int currentMaxValue = 0;
+
+		for (int i = 0; i < size; i++) {
+			currentMaxValue = currentMaxValue + a[i];
+			if (result < currentMaxValue)
+				result = currentMaxValue;
+			if (currentMaxValue < 0)
+				currentMaxValue = 0;
+		}
+		return result;
+	}
+
+	/**
+	 * Fill the gicen array with random numbers betwen -10 and 10
+	 * 
+	 * @param result
+	 * @return
+	 */
 	public int[] fillArray(int[] result) {
 		for (int i = 0; i < result.length; i++) {
 			result[i] = createRandomNumber();
@@ -23,6 +61,11 @@ public class Config03 {
 		return result;
 	}
 
+	/**
+	 * Create a random number betwen -10 and 10
+	 * 
+	 * @return
+	 */
 	public int createRandomNumber() {
 		Random r = new Random();
 		int Low = -10;
